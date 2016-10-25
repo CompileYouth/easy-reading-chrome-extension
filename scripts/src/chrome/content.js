b6d22882ec012a2b591cdfe56a86c08126941e47
CompileYouth <CompileYouth@163.com>
2016-10-25 11:45:08 +0800

    aa.

--- scripts/src/chrome/content.js
+++ scripts/src/chrome/content.js
@@ -2,6 +2,18 @@ $(() => {
     $(".top-banner-ad-container").remove();
     $(".ad-slot-container").remove();
 
-    $(document.body).append($(`<div id="shanbay" />`));
+    const $sb = $(`<div id="shanbay" />`);
+    $(document.body).append($sb);
 
+    $sb.click((e) => {
+        //$sb.addClass("readable");
+
+        $("body > .l-side-margins, body > footer, body > header").hide();
+
+        // const $article = $("#article");
+        //
+        // $(document.body).remove();
+        //
+        // $sb.append($article);
+    });
 });
