d336a6d1941617f24361ff93ea34a8a3ca5f51df
CompileYouth <CompileYouth@163.com>
2016-10-25 16:53:48 +0800

    Just show main contents.

--- scripts/src/chrome/content.js
+++ scripts/src/chrome/content.js
@@ -5,15 +5,35 @@ $(() => {
     const $sb = $(`<div id="shanbay" />`);
     $(document.body).append($sb);
 
+    const hiddenEles = [
+        "body > header",
+        "body > .l-side-margins > .content-footer",
+        "#article > header > div.content__header .content__labels",
+        "#article .content__main .content__secondary-column",
+        "#article > .content__main .content__meta-container",
+        "#article .content__main .gs-container .content__main-column .content__article-body .element",
+        "#article > .content__main .submeta",
+        "body > footer"
+    ];
+
     $sb.click((e) => {
-        //$sb.addClass("readable");
+        $sb.addClass("toReadable");
+        $(`.l-side-margins`).css({
+            opacity: 0,
+            "z-index": 10000
+        });
 
-        $("body > .l-side-margins, body > footer, body > header").hide();
+        setTimeout(() => {
+            for (let i = 0; i < hiddenEles.length; i++) {
+                $(hiddenEles[i]).hide();
+            }
+            $sb.addClass("readable");
 
-        // const $article = $("#article");
-        //
-        // $(document.body).remove();
-        //
-        // $sb.append($article);
+
+            setTimeout(() => {
+                $(`.l-side-margins`).addClass("reading");
+            }, 500);
+        }, 800);
+
     });
 });
