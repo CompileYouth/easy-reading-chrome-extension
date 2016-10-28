3e3e2974c008ad92af7094ee039e1acb77414d99
CompileYouth <CompileYouth@163.com>
2016-10-28 15:36:45 +0800

    Reconstruct.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: {
+        vendor: [ "jquery" ],
+        er: [ "./src/scripts/index.js", "./src/style/index.css" ]
+    },
+    output: {
+        path: path.resolve("./"),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            },
+            {
+                test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=8192'
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.ProvidePlugin({
+            "$": "jquery",
+            "jQuery": "jquery"
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            filename: "vendor.js",
+            minChunks: Infinity
+        }),
+        new ExtractTextPlugin("./bundle.css")
+    ],
+};
